11c97d7aefd75feceabe3c0635f746db21c93311
e.bondarev <e.bondarev@dodobrand.io>
2025-09-13 10:11:42 +0300

    Fix test example

--- app/src/main/java/com/example/podlodka/fpsample/testing/model/FulfillmentPlan.kt
+++ app/src/main/java/com/example/podlodka/fpsample/testing/model/FulfillmentPlan.kt
@@ -1,7 +1,5 @@
 package com.example.podlodka.fpsample.testing.model
 
-import com.example.podlodka.fpsample.testing.Order
-
 data class FulfillmentPlan(
   val fulfilled: List<Order>,
   val pending: List<Order>
