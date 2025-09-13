f073f1f4683ed8e87b1e12b7f0297880055ee00f
e.bondarev <e.bondarev@dodobrand.io>
2025-09-13 10:12:11 +0300

    Fix test example

--- app/src/main/java/com/example/podlodka/fpsample/testing/service/OrderService.kt
+++ app/src/main/java/com/example/podlodka/fpsample/testing/service/OrderService.kt
@@ -1,6 +1,6 @@
 package com.example.podlodka.fpsample.testing.service
 
-import com.example.podlodka.fpsample.testing.Order
+import com.example.podlodka.fpsample.testing.model.Order
 
 interface OrderService {
   fun getPendingOrders(): List<Order>
